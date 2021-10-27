e0b6bc108ea21738832dd98c3fd3d1045837f54d
Nir Funt <Antiflag77@gmail.com>
2021-10-27 11:25:56 +0300

    rendering images galley, no service yet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+function init () {
+    renderGrid();
+}
+
+function renderGrid() {
+   var elGalleryContainer = document.querySelector('.gallery-container');
+   console.log(elGalleryContainer);
+   var htmlStr = '';
+   for (var i = 1; i <= 18; i++) {
+    htmlStr +=`<img src="img-square/${i}.jpg" class="square-img" id="myID">`
+   }
+   elGalleryContainer.innerHTML = htmlStr;
+}
